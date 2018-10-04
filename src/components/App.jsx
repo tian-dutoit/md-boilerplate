9b6f7bec8142f78542ffaba3958623b5f5f29027
Tian du Toit <tian@hmsnz.com>
2018-10-04 14:58:39 +1300

    Added material-ui example tables and updated babelrc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Example from './Example'
+import PaginationTable from './PaginationTable'
+import StyleTable from './StyleTable'
+import SortTable from './SortTable'
 import "../main.css"
 
 const App = () => (
   <div className='app-container'>
     <Example /> 
+    <PaginationTable /> 
+    <StyleTable /> 
+    <SortTable /> 
   </div>
 )
 
